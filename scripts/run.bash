ef11dfefebee60d7ce5c4f110d79219603868b7e
allanjie <allanmcgrady@gmail.com>
2019-01-04 20:17:03 +0800

    script has a bug

--- scripts/run.bash
+++ scripts/run.bash
@@ -16,8 +16,8 @@ for (( d=0; d<${#datasets[@]}; d++  )) do
     dataset=${datasets[$d]}
     for (( h=0; h<${#heads[@]}; h++  )) do
         head=${heads[$h]}
-        python3 dep_main.py --dynet-seed ${seed} --use_head ${head} \
-          --dataset ${datasets}  > logs/${dataset}_head_${head}.log 2>&1
+        python3.6 dep_main.py --dynet-seed ${seed} --use_head ${head} \
+          --dataset ${dataset}  > logs/${dataset}_head_${head}.log 2>&1
     done
 
 done
