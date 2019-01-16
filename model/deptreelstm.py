60e3c8e6d02b7ccb55e1196ce2d1d22612149eec
Allan <allanmcgrady@gmail.com>
2019-01-16 09:42:38 +0800

    add code for bidirection

--- model/deptreelstm.py
+++ model/deptreelstm.py
@@ -20,15 +20,18 @@ class DepTreeLSTM():
         self.BS = [model.add_parameters((wdim)) for _ in "ifou"]
         self.US = [model.add_parameters((hdim, hdim)) for _ in "ifou"]
 
-    def expr_for_tree(self, tree, seq_h, final_h):
+    def expr_for_tree(self, tree, seq_h, final_h, final_c):
         x = seq_h[tree.pos]
         if tree.is_leaf():
+            if final_h[tree.pos] is not None:
+                return  final_h[tree.pos], final_c[tree.pos] ## useful for bidrectional tree.
             i = dy.logistic(dy.affine_transform([self.BS[0], self.WS[0], x]))
             o = dy.logistic(dy.affine_transform([self.BS[2], self.WS[2], x]))
             u = dy.tanh(dy.affine_transform([self.BS[3], self.WS[3], x]))
             c = dy.cmult(i, u)
             h = dy.cmult(o, dy.tanh(c))
             final_h[tree.pos] = h
+            final_c[tree.pos] = c
             return h, c
 
         uis = dy.concatenate([self.US[0]] * len(tree.children), 1)
@@ -50,6 +53,7 @@ class DepTreeLSTM():
         c = dy.cmult(i, u) + dy.sum_dim(dy.concatenate([dy.cmult(f, c) for f,c in zip(fs, cs)], 1), [1])
         h = dy.cmult(o, dy.tanh(c))
         final_h[tree.pos] = h
+        final_c[tree.pos] = c
         return h, c
 
 if __name__ == "__main__":
