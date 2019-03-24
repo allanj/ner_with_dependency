850c9700feab7d3b6c75c7a0c899c8f5530ffe7a
allanjie <allanmcgrady@gmail.com>
2019-03-24 12:46:12 +0800

    elmo embedding

--- preprocess/preelmo.py
+++ preprocess/preelmo.py
@@ -8,10 +8,14 @@ from allennlp.commands.elmo import ElmoEmbedder
 import pickle
 
 
-def parse_sentence(elmo, words, average:bool=True):
+def parse_sentence(elmo, words, mode:str="average"):
     vectors = elmo.embed_sentence(words)
-    if average:
+    if mode == "average":
         return np.average(vectors, 0)
+    elif mode == 'weighted_average':
+        return np.swapaxes(vectors, 0, 1)
+    elif mode == 'last':
+        return vectors[-1, :, :]
     else:
         return vectors
 
@@ -21,26 +25,27 @@ def load_elmo():
 
 
 
-def read_parse_write(elmo, infile, outfile):
+def read_parse_write(elmo, infile, outfile, mode):
     reader = Reader()
     insts = reader.read_conll(infile, -1, True)
     f = open(outfile, 'wb')
     all_vecs = []
     for inst in insts:
-        vec = parse_sentence(elmo, inst.input.words)
+        vec = parse_sentence(elmo, inst.input.words, mode=mode)
         all_vecs.append(vec)
     pickle.dump(all_vecs, f)
     f.close()
 
 
 elmo = load_elmo()
-file = "../data/conll2003/train.conllx"
-outfile = file + ".elmo.vec"
-read_parse_write(elmo, file, outfile)
-file = "../data/conll2003/dev.conllx"
-outfile = file + ".elmo.vec"
-read_parse_write(elmo, file, outfile)
-file = "../data/conll2003/test.conllx"
-outfile = file + ".elmo.vec"
-read_parse_write(elmo, file, outfile)
+mode= "last"
+file = "./data/conll2003/train.sd.conllx"
+outfile = file + ".elmo."+mode+".vec"
+read_parse_write(elmo, file, outfile, mode)
+file = "./data/conll2003/dev.sd.conllx"
+outfile = file + ".elmo."+mode+".vec"
+read_parse_write(elmo, file, outfile, mode)
+file = "./data/conll2003/test.sd.conllx"
+outfile = file + ".elmo."+mode+".vec"
+read_parse_write(elmo, file, outfile, mode)
 
