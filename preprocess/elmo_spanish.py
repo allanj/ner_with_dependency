ea300cbb79b48708bd76e7f0d7a0cd804d0619f8
allanjie <allanmcgrady@gmail.com>
2019-04-25 12:43:17 +0800

    preprocess file and run with limited amount of data

--- preprocess/elmo_spanish.py
+++ preprocess/elmo_spanish.py
@@ -7,7 +7,7 @@ This file should be deprecated since every time result is different.
 """
 
 
-def read_conll2002(filename:str):
+def read_conllx(filename:str):
     print(filename)
     sents = []
     with open(filename, 'r', encoding='utf-8') as f:
@@ -15,27 +15,36 @@ def read_conll2002(filename:str):
         for line in f.readlines():
             line = line.rstrip()
             if line == "":
+                if len(words) == 0:
+                    print("len is 0")
                 sents.append(words)
                 words = []
                 continue
-            word, _ = line.split()
-            words.append(word)
+            vals = line.split()
+            words.append(vals[1])
     return sents
 
 
 def context_emb(emb, sents):
-    return emb.sents2elmo(sents)
+    ## 0, word encoder:
+    ##1 for the first LSTM hidden layer
+    ## 2 for the second LSTM hidden lyaer
+    ## -1 for an average of 3 layers (default)
+    ## -2 for all 3 layers
+    return emb.sents2elmo(sents, -1)
 
 
 def read_parse_write(elmo, in_file, out_file):
-    sents = read_conll2002(in_file)
+    sents = read_conllx(in_file)
+    print("number of sentences: {} in {}".format(len(sents), in_file))
     f = open(out_file, 'wb')
-    batch_size = 1000
+    batch_size = 1
     all_vecs = []
     for idx in range(0, len(sents), batch_size):
         start = idx*batch_size
         end = (idx+1)*batch_size if (idx+1)*batch_size < len(sents) else len(sents)
-        batch_sents = sents[idx*batch_size: (idx+1)*batch_size]
+        batch_sents = sents[start: end]
+        #print(batch_sents)
         embs = context_emb(elmo, batch_sents)
         for emb in embs:
             all_vecs.append(emb)
@@ -43,18 +52,19 @@ def read_parse_write(elmo, in_file, out_file):
     f.close()
 
 
-elmo = Embedder('/Users/allanjie/Downloads/Spanish_ELMo/')
+
+elmo = Embedder('/data/allan/embeddings/Indonesian_ELMo', batch_size=1)
 # /data/allan/embeddings/Spanish_ELMo/
 
 
-# read_parse_write(elmo, "../data/conll2002/train.txt", "../data/conll2002/train.conllx.elmo.average.vec")
-# read_parse_write(elmo, "../data/conll2002/dev.txt", "../data/conll2002/dev.conllx.elmo.average.vec")
-# read_parse_write(elmo, "../data/conll2002/test.txt", "../data/conll2002/test.conllx.elmo.average.vec")
+read_parse_write(elmo, "data/indo/train.sd.conllx", "data/indo/train.conllx.elmo.vec")
+read_parse_write(elmo, "data/indo/dev.sd.conllx", "data/indo/dev.conllx.elmo.vec")
+read_parse_write(elmo, "data/indo/test.sd.conllx", "data/indo/test.conllx.elmo.vec")
 
 # sents = read_conll2002("../data/conll2002/train.txt")
-sents = [['multinacional', 'española', 'Telefónica', 'Telefónica']]
-result = context_emb(elmo, sents)
-print("something")
+# sents = [['我', '在', '中国']]
+# result = context_emb(elmo, sents)
+# print("something")
 
 
 # sents = read_conll2002("../data/conll2002/dev.txt")
